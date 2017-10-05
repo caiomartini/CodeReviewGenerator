ac32e3a4af4fbe1e2782d5e0bc23076a473a813a
caiomartini <caiomartini@hotmail.com>
2017-10-04 21:47:07 -0300

    Update code_review_generator.py

--- code_review_generator.py
+++ code_review_generator.py
@@ -21,85 +21,95 @@ def cherry_pick(gitcommand, commit):
     try:
         gitcommand.execute("git cherry-pick -n --strategy=recursive -X theirs {}".format(commit))
     except:
-        # Remove os arquivos alterados que foram removidos da branch atual
-        # e deram conflito no cherry-pick
+        # Adiciona os arquivos alterados que deram conflito no cherry-pick
         files = gitcommand.execute("git diff --name-only --diff-filter=U").split("\n")
         for file in files:
-            gitcommand.execute("git rm -f {}".format(file))
+            if file:
+                gitcommand.execute("git add {}".format(file))
 
-print("\n GERACAO DE CODE REVIEW INICIADO ")
-print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
+def criar_branch_base(gitcommand, branch_review_base, primeiro_commit):
+    try:
+        print(">> Criando Branch {} ...".format(branch_review_base))
+        gitcommand.execute("git checkout {}~1".format(primeiro_commit))
+        gitcommand.execute("git checkout -b {}".format(branch_review_base))
+        gitcommand.execute("git push origin {}".format(branch_review_base))
+        print("OK - Branch {} criada.\n".format(branch_review_base))
+    except Exception as e:
+        print("ERRO - Ocorreu um erro ao gerar a branch {}. {}\n".format(branch_review_base, str(e)))
+        print(">> Efetuando o rollback ...")
+        gitcommand.execute("git reset --hard")
+        gitcommand.execute("git checkout {}".format(branch_base))
+        remover_branch(gitcommand, branch_review_base)
+        print("OK - Rollback efetuado.")
+        exit(1)
 
-start_time = time.time()
+def criar_branch_diff(gitcommand, branch_review_diff, primeiro_commit, commits, tag):
+    try:
+        print(">> Criando Branch {} ...".format(branch_review_diff))
+        gitcommand.execute("git checkout {}~1".format(primeiro_commit))
+        gitcommand.execute("git checkout -b {}".format(branch_review_diff))
+        for commit in commits:
+            cherry_pick(gitcommand, commit)
+        gitcommand.execute("git commit -n -m \"[{}]: Criação de branch de Code Review\"".format(tag))
+        gitcommand.execute("git push origin {}".format(branch_review_diff))
+        print("OK - Branch {} criada.\n".format(branch_review_diff))
+    except Exception as e:
+        print("ERRO - Ocorreu um erro ao gerar a branch {}. {}\n".format(branch_review_diff, str(e)))
+        print(">> Efetuando o rollback ...")
+        gitcommand.execute("git cherry-pick --abort")
+        gitcommand.execute("git reset --hard")
+        gitcommand.execute("git checkout {}".format(branch_base))
+        remover_branch(gitcommand, branch_review_diff)
+        print("OK - Rollback efetuado.")
+        exit(1) 
 
-story = sys.argv[1]
-branch_base = sys.argv[2]
-branch_review_base = "CR_BASE_{}".format(story)
-branch_review_diff = "CR_DIFF_{}".format(story)
-gitcommand = git.Git("C:/Projetos/mitsui")
+def main():
+    print("\n GERACAO DE CODE REVIEW INICIADO ")
+    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
 
-print(">> Atualizando branches ...")
-gitcommand.execute("git pull --all")
-print("OK - Branches atualizadas.\n")
+    start_time = time.time()
 
-print(">> Acessando branch {} ...".format(branch_base))
-gitcommand.execute("git checkout {}".format(branch_base))
-print("OK - Branch atual {}.\n".format(branch_base))
+    repository = sys.argv[1]
+    branch_base = sys.argv[2]
+    tag = sys.argv[3]
+    branch_review_diff = "CR_DIFF_{}".format(tag)
+    gitcommand = git.Git(repository)
 
-remover_branch(gitcommand, branch_review_base)
-remover_branch(gitcommand, branch_review_diff)
+    print(">> Atualizando branches ...")
+    gitcommand.execute("git pull --all")
+    print("OK - Branches atualizadas.\n")
 
-print(">> Listando commits da estoria {} ...".format(story))
-commits = gitcommand.execute("git log --pretty=\"format:%h\" --grep=\"{}\"".format(story)).split("\n")
-first_commit = commits[-1]
-commits = list(reversed(commits))
-
-print(">> INFORMACOES DE CODE REVIEW")
-print(" - Estoria                 : {}".format(story))
-print(" - Branch Code Review Base : {}".format(branch_review_base))
-print(" - Branch Code Review Diff : {}".format(branch_review_diff))
-print(" - Lista de Commits        : {}\n".format(" ".join(commits)))
-
-try:
-    print(">> Criando Branch {} ...".format(branch_review_base))
-    gitcommand.execute("git checkout {}~1".format(first_commit))
-    gitcommand.execute("git checkout -b {}".format(branch_review_base))
-    gitcommand.execute("git push origin {}".format(branch_review_base))
-    print("OK - Branch {} criada.\n".format(branch_review_base))
-except Exception as e:
-    print("ERRO - Ocorreu um erro ao gerar a branch {}. {}\n".format(branch_review_base, str(e)))
-    print(">> Efetuando o rollback ...")
-    gitcommand.execute("git reset --hard")
+    print(">> Acessando branch {} ...".format(branch_base))
     gitcommand.execute("git checkout {}".format(branch_base))
-    remover_branch(gitcommand, branch_review_base)
-    print("OK - Rollback efetuado.")
-    exit(1)
+    print("OK - Branch atual {}.\n".format(branch_base))
 
-try:
-    print(">> Criando Branch {} ...".format(branch_review_diff))
-    gitcommand.execute("git checkout {}~1".format(first_commit))
-    gitcommand.execute("git checkout -b {}".format(branch_review_diff))
-    for commit in commits:
-        cherry_pick(gitcommand, commit)
-    gitcommand.execute("git commit -m \"[{}]: Criação de branch de Code Review\"".format(story))
-    gitcommand.execute("git push origin {}".format(branch_review_diff))
-    print("OK - Branch {} criada.\n".format(branch_review_diff))
-except Exception as e:
-    print("ERRO - Ocorreu um erro ao gerar a branch {}. {}\n".format(branch_review_diff, str(e)))
-    print(">> Efetuando o rollback ...")
-    gitcommand.execute("git cherry-pick --abort")
-    gitcommand.execute("git reset --hard")
-    gitcommand.execute("git checkout {}".format(branch_base))
     remover_branch(gitcommand, branch_review_diff)
-    print("OK - Rollback efetuado.")
-    exit(1)
 
-gitcommand.execute("git checkout {}".format(branch_base))
+    print(">> Listando commits da estoria {} ...".format(tag))
+    commits = gitcommand.execute("git log --pretty=\"format:%h\" --grep=\"{}\"".format(tag)).split("\n")
+    primeiro_commit = commits[-1]
+    commits = list(reversed(commits))
+    print("OK - Commits listados.\n")
 
-webbrowser.open("https://bitbucket.org/ciandt_it/mitsui/branch/{}?dest={}#diff".format(branch_review_diff, branch_review_base), new=2)
+    print(">> Informacoes de Code Review")
+    print(" - Tag                : {}".format(tag))
+    print(" - Branch Code Review : {}".format(branch_review_diff))
+    print(" - Lista de Commits   : {}\n".format(" ".join(commits)))
 
-hours, rem = divmod(time.time() - start_time, 3600)
-minutes, seconds = divmod(rem, 60)
+    criar_branch_diff(gitcommand, branch_review_diff, primeiro_commit, commits, tag)
 
-print(">> Geracao de Code Review Finalizado.")
-print(">> Tempo de execucao: {:0>2}:{:0>2}:{:05.2f}\n".format(int(hours),int(minutes),seconds))
+    ultimo_commit = gitcommand.execute("git rev-parse HEAD")
+
+    gitcommand.execute("git checkout {}".format(branch_base))
+
+    link = "https://bitbucket.org/ciandt_it/mitsui/commits/{}?at={}".format(ultimo_commit, branch_review_diff)
+    webbrowser.open(link)
+
+    hours, rem = divmod(time.time() - start_time, 3600)
+    minutes, seconds = divmod(rem, 60)
+
+    print(">> Geracao de Code Review finalizado.")
+    print(">> Link para analise: {}".format(link))
+    print(">> Tempo de execucao: {:0>2}:{:0>2}:{:05.2f}\n".format(int(hours),int(minutes),seconds))
+
+main()
